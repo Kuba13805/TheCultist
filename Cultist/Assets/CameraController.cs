726f4fd92edc1300aa09cfa7cfc22d0699369462
Kuba13805 <79981760+Kuba13805@users.noreply.github.com>
2023-03-11 14:19:34 +0100

    Poprawione sterowanie kamery
    Rotacja myszką i zoom scrollem

--- Cultist/Assets/CameraController.cs
+++ Cultist/Assets/CameraController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -10,13 +11,21 @@ public class CameraController : MonoBehaviour
     public float movementTime;
     public float rotationAmount;
     public Vector3 zoomAmount;
-    
+    public float mouseRotationSpeed;
 
     public Vector3 newPosition;
 
     public Quaternion newRotation;
 
     public Vector3 newZoom;
+
+    public Vector3 dragStartPosition;
+
+    public Vector3 dragCurrentPosition;
+
+    public Vector3 rotateStartPosition;
+
+    public Vector3 rotateCurrentPosition;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,12 +37,55 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        HandleMovementInput();
-        HandleRotation();
-        HandleZoom();
+        HandleMouseInput();
+        HandleKeyboardInput();
     }
 
-    private void HandleMovementInput()
+    private void HandleMouseInput()
+    {
+        HandleMouseRotation();
+        HandleMouseZoom();
+    }
+
+    private void HandleMouseRotation()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            rotateStartPosition = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            rotateCurrentPosition = Input.mousePosition;
+
+            Vector3 difference = rotateStartPosition - rotateCurrentPosition;
+
+            rotateStartPosition = rotateCurrentPosition;
+            
+            newRotation *= quaternion.Euler(Vector3.up * (-difference.x / mouseRotationSpeed));
+        }
+    }
+
+    private void HandleMouseZoom()
+    {
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            newZoom -= zoomAmount;
+        }
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            newZoom += zoomAmount;
+        }
+
+        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * movementTime);
+    }
+    private void HandleKeyboardInput()
+    {
+        HandleKeyboardMovement();
+        HandleKeyboardRotation();
+        HandleKeyboardZoom();
+    }
+    private void HandleKeyboardMovement()
     {
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
@@ -51,11 +103,11 @@ public class CameraController : MonoBehaviour
         {
             newPosition += (transform.right * -movementSpeed);
         }
-
+        
         transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
     }
 
-    private void HandleRotation()
+    private void HandleKeyboardRotation()
     {
         if (Input.GetKey(KeyCode.Q))
         {
@@ -68,17 +120,8 @@ public class CameraController : MonoBehaviour
         transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * movementTime);
     }
 
-    private void HandleZoom()
+    private void HandleKeyboardZoom()
     {
-        if (Input.mouseScrollDelta.y > 0)
-        {
-            newZoom -= zoomAmount;
-        }
-        if (Input.mouseScrollDelta.y < 0)
-        {
-            newZoom += zoomAmount;
-        }
-
         if (Input.GetKey(KeyCode.R))
         {
             newZoom += zoomAmount;
@@ -88,7 +131,7 @@ public class CameraController : MonoBehaviour
         {
             newZoom -= zoomAmount;
         }
-        
+
         cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * movementTime);
     }
 }
