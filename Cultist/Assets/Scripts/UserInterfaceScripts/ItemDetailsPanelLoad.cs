50f513dc4187468b61401499313726d9a6583691
Kuba13805 <79981760+Kuba13805@users.noreply.github.com>
2023-11-10 12:08:00 +0100

    UI update
    Dodano nowe elementy i resize paneli

--- Cultist/Assets/Scripts/UserInterfaceScripts/ItemDetailsPanelLoad.cs
+++ Cultist/Assets/Scripts/UserInterfaceScripts/ItemDetailsPanelLoad.cs
@@ -8,6 +8,15 @@ using UnityEngine.UI;
 
 public class ItemDetailsPanelLoad : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI itemName;
+    
+    [SerializeField] private Image itemIcon;
+    
+    [SerializeField] private TextMeshProUGUI itemValue;
+    
+    [SerializeField] private TextMeshProUGUI itemEffects;
+    
+    [SerializeField] private TextMeshProUGUI itemDesc;
     private void OnDisable()
     {
         Destroy(gameObject);
@@ -15,15 +24,15 @@ public class ItemDetailsPanelLoad : MonoBehaviour
 
     public void LoadItemDetails(BaseItem item)
     {
-        transform.Find("ItemDesc").GetComponent<TextMeshProUGUI>().text = item.itemDesc;
+        itemDesc.text = item.itemDesc;
 
         var itemDisplay = transform.Find("ItemDisplay");
-        itemDisplay.Find("ItemName").GetComponent<TextMeshProUGUI>().text = item.itemName;
-        itemDisplay.Find("ItemIcon").GetComponent<Image>().sprite = item.icon;
+        itemName.text = item.itemName;
+        itemIcon.sprite = item.icon;
 
-        transform.Find("ItemValueDisplay").GetComponentInChildren<TextMeshProUGUI>().text = item.value.ToString();
+        itemValue.text = item.value.ToString();
         
-        transform.Find("ItemEffectsDisplay").GetComponentInChildren<TextMeshProUGUI>().text = CreateTextForItemEffects(item.effectsOnItem);
+        itemEffects.text = CreateTextForItemEffects(item.effectsOnItem);
     }
 
     private string CreateTextForItemEffects(List<ItemMultipleEffects> multipleEffectsList)
